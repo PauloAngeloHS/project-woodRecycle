522da15275f8761981fe7e2585bd0eaedbea91b1
jmmjml <116905479+jmmjml@users.noreply.github.com>
2023-09-28 10:58:17 +0000

    hi

--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,9 +1,40 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, ImageBackground ,Pressable } from "react-native";
 import styles from "./styles"
-export default function Home() {
+
+import background from "../../assets/images/backgroundMadeira.jpeg"
+import reciclagem from "../../assets/images/simboloRecicladem.png"
+export default function Home({ navigation }) {
   return (
-    <View>
-      <Text>Banana</Text>
+    <View style={styles.container}>
+      <ImageBackground 
+        source={background} 
+        resizeMode="cover" 
+        style={{width: '100%', height: "100%"}}
+       >
+        <View style={styles.content}>
+        <Image
+        style={styles.image}
+        source={reciclagem}
+        contentFit="cover"
+      />
+      <Text></Text>
+        <Pressable
+            style={{
+              backgroundColor: '#b1deef',
+              width: 140,
+              height: 55,
+              alignItems: 'center',
+              justifyContent: 'center',
+              borderRadius: 30,
+            }}
+            onPress={() => navigation.navigate('about')}>
+            <Text
+              style={{ color: '#044e8f', fontWeight: 'bold', fontSize: 20 }}>
+              Sobre
+            </Text>
+          </Pressable>
+        </View>  
+      </ImageBackground>    
     </View>
   );
 }
--- screens/Home/styles.js
+++ screens/Home/styles.js
@@ -1,2 +1,18 @@
 import { StyleSheet } from "react-native";
-export default styles = new StyleSheet.create({});
+export default styles = new StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#044e8f",
+        justifyContent: "center",
+        alignItems: "center",
+      },
+      content: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItens: 'center',
+      },
+      image:{
+        width:'50%',
+        height: '30%',
+      },
+});
