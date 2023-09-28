7e333e5b68bfe796d3ba64e9ceea88e28ba31a19
jmmjml <116905479+jmmjml@users.noreply.github.com>
2023-09-28 17:16:48 +0000

    vorteiii

--- screens/About/index.js
+++ screens/About/index.js
@@ -1,5 +1,6 @@
-import { View, Text, Image, Pressable } from "react-native";
+import { View, Text, Image, Pressable,ImageBackground  } from "react-native";
 import styles from "./styles"
+import background from "../../assets/images/backgroundMadeira.jpeg"
 import placeHolder from "../../assets/images/placeHolder.png";
 function Person(props) {
  return ( <View style={styles.personWrapper}>
@@ -10,16 +11,28 @@ function Person(props) {
 }
 export default function About({ navigation }) {
   return <View>
-    <View style={styles.persons}>
-      <Person name="Jose Miguel" job="Organizador" image={placeHolder}></Person>
-      <Person name="Sofia" job="Organizador" image={placeHolder}></Person>
-      <Person name="Murilo" job="Organizador" image={placeHolder}></Person>
-      <Person name="Paulo" job="Organizador" image={placeHolder}></Person>
-      <Person name="Miguel" job="Organizador" image={placeHolder}></Person>
-      <Person name="Luis Felipe" job="Organizador" image={placeHolder}></Person>
-    </View>
-    <Pressable onPress={() =>{navigation.navigate('Home')}} style={styles.buttonWrapper}>
-      <Text style={styles.buttonText}>Voltar</Text>
-    </Pressable>
+    <ImageBackground 
+        source={background} 
+        resizeMode="cover" 
+        style={{width: '100%', height: "100%"}}
+    >
+      <View style={styles.persons}>
+        <View style={styles.espaco}>
+        <Person name="Jose Miguel" job="Ajudou nos custos, na montagem do aplicativo e na montagem dos itens reciclados." image={placeHolder}></Person>
+        </View>
+        <Person name="Sofia" job="Montadora principal dos itens, trouxe o que iriamos precisar usar." image={placeHolder}></Person>
+        <View style={styles.espaco}>
+        <Person name="Murilo" job="Ajudou nos custos, ficou responsável por pintar os itens." image={placeHolder}></Person>
+        </View>
+        <Person name="Paulo" job="Responsável principal pelo aplicativo." image={placeHolder}></Person>
+        <View style={styles.espaco}>
+        <Person name="Miguel" job="Responsável por comprar as tintas e por pintar os itens." image={placeHolder}></Person>
+        </View>
+        <Person name="Luis Felipe" job="Ajudou nos custos, na montagem e na pintura dos itens." image={placeHolder}></Person>
+      </View>
+      <Pressable onPress={() =>{navigation.navigate('Home')}} style={styles.buttonWrapper}>
+        <Text style={styles.buttonText}>Voltar</Text>
+      </Pressable>
+    </ImageBackground>
   </View>;
 }
