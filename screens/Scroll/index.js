831bd53a27883a0ae1c350aa37268b3ea006b622
PauloAngeloHS <134169822+PauloAngeloHS@users.noreply.github.com>
2023-09-28 11:22:56 +0000

    a

--- screens/Scroll/index.js
+++ screens/Scroll/index.js
@@ -1,31 +1,27 @@
-import { View, Text, Image, ImageBackground ,Pressable } from "react-native";
+import { View, Text, Image ,Pressable } from "react-native";
 import styles from "./styles"
-export default function Home({ navigation }) {
+function Slide(props){
+
+  return (<View>
+    
+  </View>)
+
+}
+function FinishButton(){
+  return (<Pressable>
+    
+  </Pressable>)
+}
+export default function Scroll({ navigation }) {
   return (
     <View style={styles.container}>
-      <ImageBackground 
-        // source={!sexo ? imagem1 : imagem2} 
-        resizeMode="cover" 
-        style={{width: '100%', height: "100%"}}
-       >
-        <View style={styles.content}>
-        <Pressable
-            style={{
-              backgroundColor: '#b1deef',
-              width: 140,
-              height: 55,
-              alignItems: 'center',
-              justifyContent: 'center',
-              borderRadius: 30,
-            }}
-            onPress={() => navigation.navigate('about')}>
-            <Text
-              style={{ color: '#044e8f', fontWeight: 'bold', fontSize: 20 }}>
-              Sobre
-            </Text>
-          </Pressable>
-        </View>  
-      </ImageBackground>    
+      
+     <View>
+      <View>
+
+      </View>
+      
+     </View>
     </View>
   );
 }
