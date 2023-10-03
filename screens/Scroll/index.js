91ca704594e877de4bb2e595a3457c99f2948469
PauloAngeloHS <134169822+PauloAngeloHS@users.noreply.github.com>
2023-10-03 14:57:50 +0000

    test

--- screens/Scroll/index.js
+++ screens/Scroll/index.js
@@ -1,7 +1,7 @@
-import { View, Text, Image ,Pressable } from "react-native";
+import { View, Text, Image ,Pressable, StyleSheet} from "react-native";
 import styles from "./styles"
 function Slide(props){
-  return (<View>
+  return (<View style={[slidesStyle,slideStyles[props.index]]}>
     <Text>{props.title}</Text>
     <Image source={props.image}/>
     <Text>{props.children}</Text>
@@ -13,27 +13,39 @@ function FinishButton(){
     
   </Pressable>)
 }
-function Index(props){
-  let index = () => {
-    return <Pressable>
+function Indexes(props){
+  // props slides
+  let initialArr = new Array(props.amount)
+  
+  function Index(props) {
+    return <Pressable onPress={()=>{
+      slideStyles.forEach(element => {
+        element.value = {
+          display: "none"
+        } 
+      });
+      slideStyles[props.id] = {
+        display: "block"
+      }
+    }}>
 
     </Pressable>
   }
-  for(let index = 0;index< props.number;index++){
-    
-  }
-  return (<View>
-
-  </View>)
+  let Indexes  = initialArr.map( props , key => {
+    return (<Index id={key} key={key}/>)
+  })
+  return Indexes;
 }
 export default function Scroll({ navigation }) {
+  let slidesStyle
+  let slideStyles = new Array(10)
   return (
     <View style={styles.container}>
       <View>
 
       </View>
      <View>
-      <Index/>
+      <Indexes slides={0}/>
       
      </View>
     </View>
